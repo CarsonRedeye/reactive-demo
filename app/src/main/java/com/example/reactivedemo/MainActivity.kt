5d54b4c8527f3a2b52e4c30d263abd28722e6172
Carson Holzheimer <carson@redeye.co>
2022-04-06 20:30:34 +1000

    Callbacks with suspend unused

--- app/src/main/java/com/example/reactivedemo/MainActivity.kt
+++ app/src/main/java/com/example/reactivedemo/MainActivity.kt
@@ -11,54 +11,43 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import reactivecircus.flowbinding.android.view.clicks
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var headingTextView: TextView
+    private lateinit var dogButton: Button
+    private lateinit var image: ImageView
+    private lateinit var urlTextView: TextView
+
+    private var imageUrl = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val headingTextView = findViewById<TextView>(R.id.animated_text)
-        val button = findViewById<Button>(R.id.my_button)
-        val image = findViewById<ImageView>(R.id.my_image)
-        val urlTextView = findViewById<TextView>(R.id.image_url_text)
+
+        headingTextView = findViewById(R.id.animated_text)
+        dogButton = findViewById(R.id.request_dog_button)
+        image = findViewById(R.id.my_image)
+        urlTextView = findViewById(R.id.image_url_text)
 
         animateText(headingTextView)
 
-        button.setOnClickListener {
+        dogButton.clicks().map { }
+        dogButton.setOnClickListener {
             fetchRandomDog { dog ->
-                urlTextView.text = dog.message
-                fetchImage(dog.message) { bitmap ->
+                imageUrl = dog.message
+                fetchImage(imageUrl) { bitmap ->
+                    urlTextView.text = imageUrl
                     image.setImageBitmap(bitmap)
                 }
             }
         }
     }
-
-    private fun animateText(textView: TextView) {
-        val rotate = RotateAnimation(
-            0f, 360f,
-            Animation.RELATIVE_TO_SELF, 0.5f,
-            Animation.RELATIVE_TO_SELF, 0.5f
-        )
-
-        rotate.duration = 1500
-        rotate.repeatCount = Animation.INFINITE
-        textView.startAnimation(rotate)
-    }
-
-
-//    private fun makeSlowRequestCallback(responseCallback: (Int) -> Unit) {
-//        GlobalScope.launch {
-//            val response: HttpResponse = client.get(randomDogUrl)
-//            responseCallback(response.status.value)
-//        }
-//    }
 }
-
 
 private fun fetchRandomDog(dogCallback: (DogResponse) -> Unit) {
     MainScope().launch {
@@ -76,5 +65,29 @@ private fun fetchImage(url: String, imageCallback: (Bitmap) -> Unit) {
     }
 }
 
+private suspend fun fetchRandomDog(): DogResponse {
+    return client.get("https://dog.ceo/api/breeds/image/random")
+}
+
+private suspend fun fetchImage(url: String): Bitmap {
+    val imageResponse: HttpResponse = client.get(url)
+    val bytes = imageResponse.readBytes()
+    return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)!!
+}
+
+private fun animateText(textView: TextView) {
+    val rotate = RotateAnimation(
+        0f, 360f,
+        Animation.RELATIVE_TO_SELF, 0.5f,
+        Animation.RELATIVE_TO_SELF, 0.5f
+    )
+
+    rotate.duration = 1500
+    rotate.repeatCount = Animation.INFINITE
+    textView.startAnimation(rotate)
+}
+
 @kotlinx.serialization.Serializable
 data class DogResponse(val message: String, val status: String)
+
+data class DogView(val bitmap: Bitmap, val caption: String)
